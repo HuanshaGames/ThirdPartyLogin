24137abf12ecc875338ff503611d8ef07cdde1f4
MCyiqiehuanying <60646086+yiqiehuanying@users.noreply.github.com>
2022-02-11 14:24:52 +0800

    优化代码格式

--- login.php
+++ login.php
@@ -21,10 +21,10 @@
 <?php 
 if(!$_GET['code']){ 
     echo '
-<script>
-    window.location.href="https://passport.huanshagame.com/auth/?client_id='.$config['client_id'].'&redirect_url='.$_SERVER['REQUEST_URI'].'"
-</script>
-';
+        <script>
+            window.location.href="https://passport.huanshagame.com/auth/?client_id='.$config['client_id'].'&redirect_url='.$_SERVER['REQUEST_URI'].'"
+        </script>
+    ';
 } else {
     include 'callback.php';
 }
